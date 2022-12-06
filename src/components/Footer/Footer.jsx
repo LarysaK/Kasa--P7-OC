888ca71b72eab4864e8062bedcd607432339ad2f
LarysaK <karahieorhiilarysa@gmail.com>
2022-12-06 13:13:32 +0100

    correction footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,11 +6,10 @@ import Logo from "../Logo/Logo";
 function Footer() {
 return (
     <footer className="footer">
-        {/* Renvoie à l'accueil en cliquant dessus */
+        {/* Renvoie à l'accueil en cliquant dessus */}
         <Link className="logofooter" to="/">
         <Logo fill="#ffffff" className="logofooter" />
-    </Link>
-}
+        </Link>
         <div className="textfooter">© 2020 Kasa. All rights reserved</div>
     </footer>
 );
